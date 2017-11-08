51b6c9b53f088cc35441bb4be9eb2258289b8b3c
Tom Dinh <tomdinhnz@gmail.com>
2017-11-08 15:38:12 +1300

    add purchase challenge

--- testPython/simpleDecision.py
+++ testPython/simpleDecision.py
@@ -3,13 +3,25 @@ inputAge = int(input("What is your age? "))
 
 mature = False
 if inputAge > 18:
-    mature =True
+    mature = True
 
 if inputName.isprintable and mature:
     print("Your name is " + inputName)
-    print("Your name is " + str(inputAge))
+    print("Your age is " + str(inputAge))
 else    :
     print("Your name is " + inputName)
-    print("Your name is " + str(inputAge) + " donot go to Kroad")
+    print("Your age is " + str(inputAge) + " donot go to Kroad")
 
 print("Thanks")
+
+freeShip = False
+inputTotal = int(input("How much is your total purchase ? "))
+
+if inputTotal >= 50:
+    freeShip = True
+
+if freeShip:
+    print("Your total purchase is " + str(inputTotal) + " and you have free shipping")
+else:
+    inputTotal +=10
+    print("Your total purchase is " + str(inputTotal) + " $10 for Shipping is added")
